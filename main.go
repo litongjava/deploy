fbb00a33e26ec74b03bd3034c794c7ce7b4832ba
litongjava <litongjava@qq.com>
2024-07-26 13:48:17 +0900

    add os env and os build

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
   "os/exec"
   "path/filepath"
   "regexp"
+  "runtime"
   "strings"
   "time"
   "unicode"
@@ -122,7 +123,7 @@ func main() {
       fmt.Println("please specified local file path")
       return
     }
-    build(b)
+    build(*b)
 
     if *z != "" {
       // 设置压缩包名称和目录
@@ -145,36 +146,6 @@ func main() {
   end := time.Now().Unix()
   fmt.Println(end-start, "s")
   fmt.Println("done")
-}
-
-func build(filePath *string) {
-  file, err := os.Open(*filePath)
-  if err != nil {
-    log.Fatalln("Error opening file:", err)
-  }
-  defer file.Close()
-
-  envVariables := []string{}
-
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    line := scanner.Text()
-
-    // 检查是否是设置环境变量的命令
-    if strings.HasPrefix(line, "set ") {
-      value := line[4:]
-      envVariables = append(envVariables, value)
-      log.Println("add env variable:", value)
-      continue // 跳过执行此命令
-    }
-
-    executeCommand(line, envVariables)
-  }
-
-  if err := scanner.Err(); err != nil {
-    log.Fatalln("Error reading file:", err)
-  }
-
 }
 
 func uploadAndRun(client *http.Client, url *string, p *string, filePath *string, m *string, d *string, c *string) {
@@ -281,11 +252,90 @@ func runRemoteCmd(client *http.Client, url *string, c *string) {
   fmt.Println(string(decodedBytes))
 }
 
+func build(filePath string) {
+  file, err := os.Open(filePath)
+  if err != nil {
+    log.Fatalln("Error opening file:", err)
+  }
+  defer file.Close()
+
+  envSection := ""
+  buildSection := ""
+
+  switch runtime.GOOS {
+  case "windows":
+    envSection = "[win.env]"
+    buildSection = "[win.build]"
+  case "darwin":
+    envSection = "[mac.env]"
+    buildSection = "[mac.build]"
+  case "linux":
+    envSection = "[linux.env]"
+    buildSection = "[linux.build]"
+  default:
+    log.Fatalln("not support current os")
+    return
+  }
+
+  envVariables := []string{}
+  buildCommands := []string{}
+
+  scanner := bufio.NewScanner(file)
+  section := ""
+
+  for scanner.Scan() {
+    line := scanner.Text()
+    line = strings.TrimSpace(line)
+
+    // 跳过注释行
+    if strings.HasPrefix(line, "#") || line == "" {
+      continue
+    }
+
+    // 判断当前行是否是一个新的部分
+    if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
+      section = line
+      continue
+    }
+
+    // 根据当前部分判断如何处理当前行
+    if section == envSection {
+      if strings.HasPrefix(line, "set ") || strings.HasPrefix(line, "export ") {
+        value := strings.TrimSpace(line)
+        envVariables = append(envVariables, value)
+        log.Println("add env variable:", value)
+      }
+    } else if section == buildSection {
+      buildCommands = append(buildCommands, line)
+    }
+  }
+
+  if err := scanner.Err(); err != nil {
+    log.Fatalln("Error reading file:", err)
+  }
+
+  // 设置环境变量
+  for _, env := range envVariables {
+    keyValue := strings.SplitN(env, "=", 2)
+    if len(keyValue) == 2 {
+      os.Setenv(keyValue[0], keyValue[1])
+    }
+  }
+
+  // 执行构建命令
+  for _, command := range buildCommands {
+    executeCommand(command, envVariables)
+  }
+}
+
 // executeCommand 在指定目录下执行一条命令，并应用所有以前设置的环境变量
 func executeCommand(commandStr string, envVariables []string) {
   log.Println("Executing in", ":", commandStr)
 
   cmd := exec.Command("cmd", "/C", commandStr)
+  if runtime.GOOS != "windows" {
+    cmd = exec.Command("sh", "-c", commandStr)
+  }
 
   // 添加之前设置的环境变量到命令中
   currEnv := os.Environ()
