711592b43ded9c02b19681614d902ed68eaddccf
litongjava <litongjava@qq.com>
2024-08-04 12:20:50 +0900

    add more cmd to c9

--- main.go
+++ main.go
@@ -37,6 +37,12 @@ func main() {
   c1 := flag.String("c1", "", "full command")
   c2 := flag.String("c2", "", "full command")
   c3 := flag.String("c3", "", "full command")
+  c4 := flag.String("c4", "", "full command")
+  c5 := flag.String("c5", "", "full command")
+  c6 := flag.String("c6", "", "full command")
+  c7 := flag.String("c7", "", "full command")
+  c8 := flag.String("c8", "", "full command")
+  c9 := flag.String("c9", "", "full command")
   z := flag.String("z", "", "zip")
 
   flag.Parse()
@@ -100,18 +106,103 @@ func main() {
       *c3 = viper.GetString(*e + "." + *a + ".c3")
     }
 
-    log.Println("b:", *b)
-    log.Println("z:", *z)
-    log.Println("url:", *url)
-    log.Println("p:", *p)
-    log.Println("filePath:", *filePath)
-    log.Println("w:", *w)
-    log.Println("m:", *m)
-    log.Println("d:", *d)
-    log.Println("c1:", *c1)
-    log.Println("c2:", *c2)
-    log.Println("c3:", *c3)
-    log.Println("c:", *c)
+    if *c4 == "" {
+      *c4 = viper.GetString(*e + "." + *a + ".c4")
+    }
+
+    if *c5 == "" {
+      *c5 = viper.GetString(*e + "." + *a + ".c5")
+    }
+
+    if *c6 == "" {
+      *c6 = viper.GetString(*e + "." + *a + ".c6")
+    }
+
+    if *c7 == "" {
+      *c7 = viper.GetString(*e + "." + *a + ".c7")
+    }
+
+    if *c8 == "" {
+      *c8 = viper.GetString(*e + "." + *a + ".c8")
+    }
+
+    if *c9 == "" {
+      *c9 = viper.GetString(*e + "." + *a + ".c9")
+    }
+    if len(*b) != 0 {
+      log.Println("b:", *b)
+    }
+    if len(*z) != 0 {
+      log.Println("z:", *z)
+    }
+
+    if len(*url) != 0 {
+      log.Println("url:", *url)
+    }
+
+    if len(*p) != 0 {
+      log.Println("p:", *p)
+    }
+
+    if len(*filePath) != 0 {
+      log.Println("filePath:", *filePath)
+    }
+
+    if len(*w) != 0 {
+      log.Println("w:", *w)
+    }
+
+    if len(*m) != 0 {
+      log.Println("m:", *m)
+    }
+
+    if len(*d) != 0 {
+      log.Println("d:", *d)
+    }
+    if len(*c1) != 0 {
+      log.Println("c1:", *c1)
+    }
+
+    if len(*c2) != 0 {
+      log.Println("c2:", *c2)
+    }
+
+    if len(*c3) != 0 {
+      log.Println("c3:", *c3)
+    }
+
+    if len(*c4) != 0 {
+      log.Println("c3:", *c4)
+    }
+
+    if len(*c4) != 0 {
+      log.Println("c3:", *c4)
+    }
+
+    if len(*c5) != 0 {
+      log.Println("c3:", *c5)
+    }
+
+    if len(*c6) != 0 {
+      log.Println("c3:", *c6)
+    }
+
+    if len(*c7) != 0 {
+      log.Println("c3:", *c7)
+    }
+
+    if len(*c8) != 0 {
+      log.Println("c3:", *c8)
+    }
+
+    if len(*c9) != 0 {
+      log.Println("c3:", *c9)
+    }
+
+    if len(*c) != 0 {
+      log.Println("c:", *c)
+    }
+
   }
 
   if *a == "web" {
@@ -157,7 +248,7 @@ func main() {
       }
       Zip(split[0], split[1], excludeFile)
     }
-    uploadAndRun(client, url, p, filePath, w, m, d, c, c1, c2, c3)
+    uploadAndRun(client, url, p, filePath, w, m, d, c, c1, c2, c3, c4, c5, c6, c7, c8, c9)
   } else if strings.HasSuffix(*url, "web/") {
     log.Println("web")
     runRemoteCmd(client, url, c)
@@ -167,7 +258,8 @@ func main() {
   fmt.Println("done")
 }
 
-func uploadAndRun(client *http.Client, url *string, p *string, filePath *string, w *string, m *string, d *string, c *string, c1 *string, c2 *string, c3 *string) {
+func uploadAndRun(client *http.Client, url *string, p *string, filePath *string, w *string, m *string, d *string, c *string, c1 *string, c2 *string, c3 *string,
+  c4 *string, c5 *string, c6 *string, c7 *string, c8 *string, c9 *string) {
   var file, errFile1 = os.Open(*filePath)
   if errFile1 != nil {
     log.Fatalln(errFile1)
@@ -219,6 +311,26 @@ func uploadAndRun(client *http.Client, url *string, p *string, filePath *string,
   }
   if len(*c3) != 0 {
     _ = writer.WriteField("c3", *c3)
+  }
+  if len(*c4) != 0 {
+    _ = writer.WriteField("c3", *c4)
+  }
+
+  if len(*c5) != 0 {
+    _ = writer.WriteField("c3", *c5)
+  }
+  if len(*c6) != 0 {
+    _ = writer.WriteField("c3", *c6)
+  }
+  if len(*c7) != 0 {
+    _ = writer.WriteField("c3", *c7)
+  }
+  if len(*c8) != 0 {
+    _ = writer.WriteField("c3", *c8)
+  }
+
+  if len(*c9) != 0 {
+    _ = writer.WriteField("c3", *c9)
   }
 
   err = writer.Close()
